cceaa0a13e8fd5fc8425be855556c858f355d85b
edilson santiago <edilsonsantiagoe7@gmail.com>
2024-02-05 09:43:28 -0300

    program parent

--- curso-java/trabalhandoComArquivos/informacoesDoCaminhoDoArquivo/ProgramParent.java
+++ curso-java/trabalhandoComArquivos/informacoesDoCaminhoDoArquivo/ProgramParent.java
@@ -1,10 +1,24 @@
 package informacoesDoCaminhoDoArquivo;
 
+import java.io.File;
+import java.util.Scanner;
+
 public class ProgramParent {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter a file path: ");
+		String strPath = sc.nextLine();
+		
+		File path = new File(strPath);
+		
+		System.out.println("getName: " + path.getName());//Nome do arquivo apenas e descarta o caminho completo.
+		System.out.println("getParent: " + path.getParent());//Caminho sem o arquivo
+		System.out.println("getParent: " + path.getPath()); //Caminho completo
+		
+		
 	}
 
 }
